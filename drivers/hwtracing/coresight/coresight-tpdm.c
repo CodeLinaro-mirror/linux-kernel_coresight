f435c7b8d910469fdfb29cfc98327434214c2920
Mao Jinlong <quic_jinlmao@quicinc.com>
2023-02-22 22:06:52 -0800

    coresight-tpdm: Add support to configure CMB collection mode
    TPDM CMB subunits support two forms of CMB data set element creation:
    continuous and trace-on-change collection mode. Continuous change
    creates CMB data set elements on every CMBCLK edge. Trace-on-change
    creates CMB data set elements only when a new data set element differs
    in value from the previous element in a CMB data set. Set CMB_CR.MODE
    to 0 for continuous CMB collection mode. Set CMB_CR.MODE to 1 for
    trace-on-change CMB collection mode
    
    Signed-off-by: Mao Jinlong <quic_jinlmao@quicinc.com>

--- drivers/hwtracing/coresight/coresight-tpdm.c
+++ drivers/hwtracing/coresight/coresight-tpdm.c
@@ -37,6 +37,22 @@ static umode_t tpdm_dsb_is_visible(struct kobject *kobj,
 	return 0;
 }
 
+static umode_t tpdm_cmb_is_visible(struct kobject *kobj,
+							struct attribute *attr, int n)
+{
+	struct device *dev = kobj_to_dev(kobj);
+	struct tpdm_drvdata *drvdata = dev_get_drvdata(dev->parent);
+
+	if (drvdata) {
+		if (drvdata->datasets & TPDM_PIDR0_DS_CMB)
+			return attr->mode;
+		else
+			return 0;
+	}
+
+	return 0;
+}
+
 static void tpdm_enable_dsb(struct tpdm_drvdata *drvdata)
 {
 	u32 val, mode, i;
@@ -112,9 +128,18 @@ static void tpdm_enable_cmb(struct tpdm_drvdata *drvdata)
 	u32 val;
 
 	val = readl_relaxed(drvdata->base + TPDM_CMB_CR);
-	val |= TPDM_CMB_CR_ENA;
+	/*
+	 * Set to 0 for continuous CMB collection mode,
+	 * 1 for trace-on-change CMB collection mode.
+	 */
+	if (drvdata->cmb->trace_mode)
+		val |= TPDM_CMB_CR_MODE;
+	else
+		val &= ~TPDM_CMB_CR_MODE;
 
 	/* Set the enable bit of CMB control register to 1 */
+	val |= TPDM_CMB_CR_ENA;
+
 	writel_relaxed(val, drvdata->base + TPDM_CMB_CR);
 }
 
@@ -237,6 +262,12 @@ static int tpdm_datasets_alloc(struct tpdm_drvdata *drvdata)
 			return -ENOMEM;
 	}
 
+	if (drvdata->datasets & TPDM_PIDR0_DS_CMB) {
+		drvdata->cmb = devm_kzalloc(drvdata->dev, sizeof(*drvdata->cmb),
+					    GFP_KERNEL);
+		if (!drvdata->cmb)
+			return -ENOMEM;
+	}
 	return 0;
 }
 
@@ -785,6 +816,38 @@ static ssize_t dsb_trig_ts_store(struct device *dev,
 	return size;
 }
 static DEVICE_ATTR_RW(dsb_trig_ts);
+
+static ssize_t cmb_mode_show(struct device *dev,
+				  struct device_attribute *attr,
+				  char *buf)
+{
+	struct tpdm_drvdata *drvdata = dev_get_drvdata(dev->parent);
+
+	return scnprintf(buf, PAGE_SIZE, "trace_mode: %s \n",
+			 drvdata->cmb->trace_mode ?
+			 "trace_on_change" : "continuous");
+}
+
+static ssize_t cmb_mode_store(struct device *dev,
+				   struct device_attribute *attr,
+				   const char *buf,
+				   size_t size)
+{
+	struct tpdm_drvdata *drvdata = dev_get_drvdata(dev->parent);
+	unsigned int trace_mode;
+	int nval;
+
+	nval = sscanf(buf, "%u", &trace_mode);
+	if (nval != 1)
+		return -EINVAL;
+
+	spin_lock(&drvdata->spinlock);
+	drvdata->cmb->trace_mode = trace_mode;
+	spin_unlock(&drvdata->spinlock);
+	return size;
+}
+static DEVICE_ATTR_RW(cmb_mode);
+
 static struct attribute *tpdm_dsb_attrs[] = {
 	&dev_attr_dsb_mode.attr,
 	&dev_attr_dsb_edge_ctrl.attr,
@@ -800,14 +863,25 @@ static struct attribute *tpdm_dsb_attrs[] = {
 	NULL,
 };
 
+static struct attribute *tpdm_cmb_attrs[] = {
+	&dev_attr_cmb_mode.attr,
+	NULL,
+};
+
 static struct attribute_group tpdm_dsb_attr_grp = {
 	.attrs = tpdm_dsb_attrs,
 	.is_visible = tpdm_dsb_is_visible,
 };
 
+static struct attribute_group tpdm_cmb_attr_grp = {
+	.attrs = tpdm_cmb_attrs,
+	.is_visible = tpdm_cmb_is_visible,
+};
+
 static const struct attribute_group *tpdm_attr_grps[] = {
 	&tpdm_attr_grp,
 	&tpdm_dsb_attr_grp,
+	&tpdm_cmb_attr_grp,
 	NULL,
 };
 
