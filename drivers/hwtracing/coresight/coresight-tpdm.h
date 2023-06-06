81dd06319d4edf93b0ad6dde818a2f52657b0131
Tao Zhang <quic_taozha@quicinc.com>
2023-06-06 18:26:50 +0800

    coresight-tpdm: Add nodes to configure pattern match output
    Add nodes to configure trigger pattern and trigger pattern mask.
    Each DSB subunit TPDM has maximum of n(n<7) XPR registers to
    configure trigger pattern match output. Eight 32 bit registers
    providing DSB interface trigger output pattern match comparison.
    And each DSB subunit TPDM has maximum of m(m<7) XPMR registers to
    configure trigger pattern mask match output. Eight 32 bit
    registers providing DSB interface trigger output pattern match
    mask.
    
    Signed-off-by: Tao Zhang <quic_taozha@quicinc.com>

--- drivers/hwtracing/coresight/coresight-tpdm.h
+++ drivers/hwtracing/coresight/coresight-tpdm.h
@@ -12,6 +12,8 @@
 /* DSB Subunit Registers */
 #define TPDM_DSB_CR		(0x780)
 #define TPDM_DSB_TIER		(0x784)
+#define TPDM_DSB_XPR(n)		(0x7C8 + (n * 4))
+#define TPDM_DSB_XPMR(n)	(0x7E8 + (n * 4))
 #define TPDM_DSB_EDCR(n)	(0x808 + (n * 4))
 #define TPDM_DSB_EDCMR(n)	(0x848 + (n * 4))
 
@@ -77,19 +79,25 @@
 #define TPDM_DSB_MAX_EDCR	16
 /* MAX number of EDCMR registers */
 #define TPDM_DSB_MAX_EDCMR	8
+/* MAX number of DSB pattern */
+#define TPDM_DSB_MAX_PATT	8
 
 /**
  * struct dsb_dataset - specifics associated to dsb dataset
  * @mode:             DSB programming mode
  * @edge_ctrl:        Save value for edge control
  * @edge_ctrl_mask:   Save value for edge control mask
+ * @trig_patt_val:    Save value for trigger pattern
+ * @trig_patt_mask:   Save value for trigger pattern mask
  * @trig_ts:          Enable/Disable trigger timestamp.
  * @trig_type:        Enable/Disable trigger type.
  */
 struct dsb_dataset {
 	u32				mode;
-	u32			edge_ctrl[TPDM_DSB_MAX_EDCR];
-	u32			edge_ctrl_mask[TPDM_DSB_MAX_EDCMR];
+	u32				edge_ctrl[TPDM_DSB_MAX_EDCR];
+	u32				edge_ctrl_mask[TPDM_DSB_MAX_EDCMR];
+	u32				trig_patt_val[TPDM_DSB_MAX_PATT];
+	u32				trig_patt_mask[TPDM_DSB_MAX_PATT];
 	bool			trig_ts;
 	bool			trig_type;
 };
