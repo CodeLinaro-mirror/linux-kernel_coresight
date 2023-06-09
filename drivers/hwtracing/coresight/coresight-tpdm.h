2f9da71ce6f2c34c0b79d1baf800d3acf4916b4f
Tao Zhang <quic_taozha@quicinc.com>
2023-06-09 15:30:08 +0800

    coresight-tpdm: Add nodes for dsb msr support
    Add the nodes for DSB subunit MSR(mux select register) support.
    The TPDM MSR (mux select register) interface is an optional
    interface and associated bank of registers per TPDM subunit.
    The intent of mux select registers is to control muxing structures
    driving the TPDM’s’ various subunit interfaces.
    
    Signed-off-by: Tao Zhang <quic_taozha@quicinc.com>

--- drivers/hwtracing/coresight/coresight-tpdm.h
+++ drivers/hwtracing/coresight/coresight-tpdm.h
@@ -18,6 +18,7 @@
 #define TPDM_DSB_XPMR(n)	(0x7E8 + (n * 4))
 #define TPDM_DSB_EDCR(n)	(0x808 + (n * 4))
 #define TPDM_DSB_EDCMR(n)	(0x848 + (n * 4))
+#define TPDM_DSB_MSR(n)		(0x980 + (n * 4))
 
 /* Enable bit for DSB subunit */
 #define TPDM_DSB_CR_ENA		BIT(0)
@@ -97,6 +98,8 @@
  * @patt_mask:        Save value for pattern mask
  * @trig_patt_val:    Save value for trigger pattern
  * @trig_patt_mask:   Save value for trigger pattern mask
+ * @msr_num           Number of MSR supported by DSB TPDM
+ * @msr               Save value for MSR
  * @patt_ts:          Enable/Disable pattern timestamp
  * @patt_type:        Set pattern type
  * @trig_ts:          Enable/Disable trigger timestamp.
@@ -110,6 +113,8 @@ struct dsb_dataset {
 	u32				patt_mask[TPDM_DSB_MAX_PATT];
 	u32				trig_patt_val[TPDM_DSB_MAX_PATT];
 	u32				trig_patt_mask[TPDM_DSB_MAX_PATT];
+	u32				msr_num;
+	u32				*msr;
 	bool			patt_ts;
 	bool			patt_type;
 	bool			trig_ts;
