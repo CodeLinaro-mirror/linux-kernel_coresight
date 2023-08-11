52935c05228975552edb8a27b5d9279294e95f08
Mao Jinlong <quic_jinlmao@quicinc.com>
2023-08-11 14:38:11 +0800

    dt-bindings: arm: Adds CoreSight CSR hardware definitions
    Adds new coresight-csr.yaml file describing the bindings required
    to define csr in the device trees.
    
    Signed-off-by: Mao Jinlong <quic_jinlmao@quicinc.com>

---
+++ include/dt-bindings/arm/coresight-csr-dt.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
+/*
+ * This header provides constants for the defined device
+ * types on CoreSight CSR.
+ */
+
+#ifndef _DT_BINDINGS_ARM_CORESIGHT_CSR_DT_H
+#define _DT_BINDINGS_ARM_CORESIGHT_CSR_DT_H
+
+#define CSR_ASSOC_DEV_ETR	1
+
+#endif /*_DT_BINDINGS_ARM_CORESIGHT_CSR_DT_H */
